8e8c5d77708fcda083b32aac0f465342d59a9317
Zer0Power <74372329+Zer0Power@users.noreply.github.com>
2022-04-05 15:59:47 +0430

    Update Bypass.js

--- Bypass.js
+++ Bypass.js
@@ -3,6 +3,7 @@
 // @include      novin-system.com
 // @version      8.5
 // @description  Fuck afaq-Lc VisibilityChange listener;D
+// @homepageURL http://0powerdev.ir/
 // @author       Great Fucking 0Power ;))))
 // @icon         https://cdn.discordapp.com/attachments/852999633385816086/955378667159171092/863b1ddc4688dec7fa2a109fa264ed74.jpg
 // @match        *://*/*
